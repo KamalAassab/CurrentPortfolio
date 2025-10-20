b7a4b72f7f024f9447ed6c29621e1cfdd9b635f4
Kamal Aassab <kamalaassab2002@gmail.com>
2025-10-20 23:41:02 +0100

    Fix AWS Amplify deployment issues: Move dependencies, remove API routes, fix TypeScript errors

--- next.config.js
+++ next.config.js
@@ -10,15 +10,6 @@ const nextConfig = {
     domains: ["user-images.githubusercontent.com", "cdn.hashnode.com", "github.com"],
     unoptimized: true,
   },
-  fontLoaders: [
-    {
-      loader: "@next/font/google",
-      options: {
-        subsets: ["latin"],
-        weight: ["400", "500", "600", "700", "800"],
-      },
-    },
-  ],
 };
 
 module.exports = nextConfig;
